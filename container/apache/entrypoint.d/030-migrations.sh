5f45c988fb8d58764e0197ed387317900dd35286
Salim Bouziane Rabhi <salim.bouziane.rabhi@mollie.com>
2022-07-27 14:54:00 +0200

    Improved visibility of Credentials

--- container/apache/entrypoint.d/030-migrations.sh
+++ container/apache/entrypoint.d/030-migrations.sh
@@ -2,3 +2,11 @@
 set -e
 
 vendor/bin/oe-eshop-db_migrate migrations:migrate
+
+echo "############################################################################"
+echo "##### Installation Complete! Please activate Mollie in Extensions Page #####"
+echo "############################################################################"
+echo "[!] Website Url 		: ${DOMAIN}"
+echo "[!] Website Admin Url : ${DOMAIN}/admin"
+echo "[!] Admin Username : admin"
+echo "[!] Admin Password : admin"
